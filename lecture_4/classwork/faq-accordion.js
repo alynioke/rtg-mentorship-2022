49614facb79f6c21f6156fed0789320ed059e3db
alinakarpelceva <alina.karpelceva@shopify.com>
2022-04-07 22:22:12 +0300

    Make project finished

--- lecture_4/classwork/faq-accordion.js
+++ lecture_4/classwork/faq-accordion.js
@@ -1,3 +1,76 @@
+/* HELPER FUNCTIONS */
+
+// Toggle CSS class to show question in HTML
+const handleOpenQuestion = (entryQuestion) => {
+  // Toggle off className right
+  entryQuestion.classList.toggle("right");
+
+  // Toggle on className bottom
+  entryQuestion.classList.toggle("bottom");
+};
+
+const handleOpenAnswer = (entryAnswer) => {
+  // Toggle off className answer-hidden
+  entryAnswer.classList.toggle("answer-hidden");
+};
+
+/**
+ * Find Questions in DOM
+ * Toggle class names to make them closed
+ */
+const handleHideOtherQuestions = (currentAnswer) => {
+  const answers = document.querySelectorAll("#faq .question-container");
+
+  answers.forEach((questionContainer) => {
+    const question = questionContainer.querySelector(".question");
+    const answer = questionContainer.querySelector(".answer");
+
+    // Skip if answer is the current answer
+    if (currentAnswer === answer) {
+      return;
+    }
+
+    // Add answer-hidden to all other answers
+    question.classList.remove("bottom");
+    question.classList.add("right");
+    answer.classList.add("answer-hidden");
+  });
+};
+
+const getAnswerContainer = (entryQuestion, entryAnswer) => {
+  const entryContainer = document.createElement("div");
+  entryContainer.className = "question-container";
+
+  entryContainer.append(entryQuestion);
+  entryContainer.append(entryAnswer);
+
+  return entryContainer;
+};
+
+const getFaqListContainer = () => {
+  // Create pseudocontainer,
+  // that does not exist in DOM yet for more performant entry append after we have generated all necessary HTML
+  return document.createElement("div");
+};
+
+const getEntryAnswer = (answer) => {
+  const answerElement = document.createElement("div");
+  answerElement.className = "answer answer-hidden";
+  answerElement.innerHTML = answer;
+
+  return answerElement;
+};
+
+const getEntryQuestion = (question) => {
+  const questionElement = document.createElement("div");
+  questionElement.className = "question chevron right question-interactive";
+  questionElement.innerHTML = question;
+
+  return questionElement;
+};
+
+/* END HELPER FUNCTIONS */
+
 const faqList = () => {
   const faqData = [
     {
@@ -89,53 +162,35 @@ const faqList = () => {
     </div>
    */
 
-  const faqElement = document.getElementById("faq");
-  faqElement.innerHTML = "";
+  const faqContainer = getFaqListContainer();
 
-  const faqContainer = document.createElement("div");
-  faqContainer.id = "faq-container";
+  const handleQuestionClick = (entryQuestion, entryAnswer) => {
+    handleHideOtherQuestions(entryAnswer);
 
-  const visible = "answer-visible";
-  const hidden = "answer-hidden";
-
-  const handleQuestionClick = (entryAnswer) => {
-    const isVisible = entryAnswer.className.includes(visible);
-
-    // Hide the answer
-    if (isVisible) {
-      entryAnswer.className = `answer ${hidden}`;
-    }
-
-    // if NOT isVisible
-    // Show the answer
-    if (!isVisible) {
-      entryAnswer.className = `answer ${visible}`;
-    }
+    handleOpenQuestion(entryQuestion);
+    handleOpenAnswer(entryAnswer);
   };
 
   faqData.forEach((faqEntry) => {
     const { question, answer } = faqEntry;
 
-    const entryContainer = document.createElement("div");
-    entryContainer.className = "question-container";
+    const entryAnswer = getEntryAnswer(answer);
+    const entryQuestion = getEntryQuestion(question);
 
-    const entryAnswer = document.createElement("div");
-    entryAnswer.className = "answer answer-hidden";
-    entryAnswer.innerHTML = answer;
-
-    const entryQuestion = document.createElement("div");
-    entryQuestion.className = "question";
-    entryQuestion.innerHTML = question;
+    // Add Question Click
     entryQuestion.onclick = () => {
-      handleQuestionClick(entryAnswer);
+      handleQuestionClick(entryQuestion, entryAnswer);
     };
 
     // Add Question and answer elements to Container
-    entryContainer.append(entryQuestion);
-    entryContainer.append(entryAnswer);
+    const entryContainer = getAnswerContainer(entryQuestion, entryAnswer);
 
     faqContainer.append(entryContainer);
   });
+
+  // Get the main container from DOM that we will be operating in.
+  const faqElement = document.getElementById("faq");
+  faqElement.innerHTML = "";
 
   faqElement.append(faqContainer);
 };
